de5fe2c4c244ba5d6bc3fc44d3cf8ddb8afd9bbb
Andre Lamurias <andrel@cs.aau.dk>
2021-12-03 11:20:58 +0100

    installation issues

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 from setuptools import Extension
 import os
 
-SETUP_METADATA = \
-               {
+SETUP_METADATA = {
     "name": "vamb",
     "description": "Variational autoencoder for metagenomic binning",
     "url": "https://github.com/RasmussenLab/vamb",
@@ -14,22 +13,17 @@ SETUP_METADATA = \
     "license": "MIT",
     "packages": find_packages(),
     "package_data": {"vamb": ["kernel.npz"]},
-    "entry_points": {'console_scripts': [
-        'vamb = vamb.__main__:main'
-        ]
-    },
-    "scripts": ['src/concatenate.py'],
-    "ext_modules": [Extension("vamb._vambtools",
-                               sources=["src/_vambtools.pyx"],
-                               language="c")],
+    "entry_points": {"console_scripts": ["vamb = vamb.__main__:main"]},
+    "scripts": ["src/concatenate.py"],
+    "ext_modules": [Extension("vamb._vambtools", sources=["src/_vambtools.pyx"], language="c")],
     "install_requires": ["numpy~=1.20", "torch~=1.8", "pysam~=0.14"],
-    "setup_requires": ['Cython~=0.29', "setuptools~=58.0"],
+    "setup_requires": ["Cython~=0.29", "setuptools>=58.0"],
     "python_requires": "~=3.7",
-    "classifiers":[
+    "classifiers": [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    }
+}
 
 setup(**SETUP_METADATA)
